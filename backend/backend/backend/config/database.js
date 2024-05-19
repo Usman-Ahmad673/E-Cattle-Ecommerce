0a41179c0495e45fb4437adab083eb99dacffa3e
Usman-Ahmad673 <60317741+Usman-Ahmad673@users.noreply.github.com>
2024-05-19 12:46:27 +0500

    Update database.js

--- backend/backend/backend/config/database.js
+++ backend/backend/backend/config/database.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  // mongoose.connect("mongodb+srv://itsusmanahmad00:sp7x46V6U7QbTMRC@cluster1.picel40.mongodb.net/?retryWrites=true&w=majority",
-  // mongoose.connect("mongodb://127.0.0.1:27017/eCattle",
-  // mongoose.connect("mongodb://127.0.0.1:27017/final",
-  mongoose
+  
     .connect(
-      "mongodb+srv://itsusmanahmad00:t3VulL1yFXffspFy@cluster0.nooqfwc.mongodb.net/?retryWrites=true&w=majority",
+      process.env.MONGO_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
