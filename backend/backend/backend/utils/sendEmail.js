a1633219b167b83e2a71e80c640d92a4f8a15346
Usman-Ahmad673 <60317741+Usman-Ahmad673@users.noreply.github.com>
2024-05-19 12:47:59 +0500

    Update sendEmail.js

--- backend/backend/backend/utils/sendEmail.js
+++ backend/backend/backend/utils/sendEmail.js
@@ -2,22 +2,19 @@ const nodeMailer = require('nodemailer')
 
 const sendEmail = async (options) => {
     const transporter = nodeMailer.createTransport({
-        // host: 'smtp.gmail.com',
-        // port: 465,
-        // service: 'gmail',
+        
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
         service: process.env.SMTP_SERVICE,
         auth:{
-            // user: 'ssimpleMailll@gmail.com',
-            // pass: 'UsmanAhmad_12345'
+            
             user:process.env.SMTP_MAIL,
             pass:process.env.SMTP_PASSWORD
         }
     })
 
     const mailOptions = { 
-        // from: 'ssimpleMailll@gmail.com',
+        
         from: process.env.SMTP_MAIL,
         to: options.email,
         subject: options.subject,
